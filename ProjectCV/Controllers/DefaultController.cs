331e8e638a5d2fffa4312fe52ebd8b69909345dd
selinnurgol <selinnur.gol@ogr.sakarya.edu.tr>
2022-12-06 14:16:46 +0300

    partial view eklendi

--- ProjectCV/Controllers/DefaultController.cs
+++ ProjectCV/Controllers/DefaultController.cs
@@ -3,14 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProjectCV.Models.Entity;
+
 
 namespace ProjectCV.Controllers
 {
     public class DefaultController : Controller
     {
+        Context db = new Context();
         public IActionResult Index()
         {
-            return View();
+            var degerler = db.TblHakkimdas.ToList();
+            return View(degerler);
         }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerims.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEgitimlerims.ToList();
+            return PartialView(egitimler);
+        }
+
     }
 }
